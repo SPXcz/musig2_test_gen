cc3d91305c3ab5afe481826b0fae58bb6b9090e8
SPXcz <ondrachu@gmail.com>
2025-02-27 14:12:19 +0100

    Keygen and noncegen produce correct results

--- test_gen.py
+++ test_gen.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import random as rnd
 import numpy as np
+import secrets
 from ref import reference as lib
 
 NO_OF_TESTS = 30
-MAX_KEY_VALUE = 2**256-1
+KEY_LENGTH = 32
 MAX_MESSAGE_LEN = 255
+
+RANDOMNESS = bytes.fromhex("0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F")
 
 def gen_keygen_data():
 
@@ -14,11 +17,11 @@ def gen_keygen_data():
 
     for _ in range(0, NO_OF_TESTS):
 
-        sk = rnd.randint(0, MAX_KEY_VALUE)
-        pk = lib.cbytes(lib.point_mul(lib.G, sk))
+        sk = secrets.token_bytes(KEY_LENGTH)
+        pk = lib.individual_pk(sk)
 
         pks.append(pk.hex().upper())
-        sks.append(lib.bytes_from_int(sk).hex().upper())
+        sks.append(sk.hex().upper())
     
     out_df = pd.DataFrame({
         'privateKey': np.array(sks),
@@ -48,14 +51,14 @@ def gen_noncegen_data():
             aggpk_grp = lib.key_agg(pk_grp).Q
 
             # Generate aggnonce and secnonce
-            secnonce, pubnonce = lib.nonce_gen(None, pk, lib.xbytes(aggpk_grp), None, None)
+            secnonce, pubnonce = lib.nonce_gen_internal(RANDOMNESS, None, pk, lib.xbytes(aggpk_grp), None, None)
         else:
             no_of_participants_grp = 0
             pk_grp = []
             aggpk_grp = None
 
             # Generate aggnonce and secnonce
-            secnonce, pubnonce = lib.nonce_gen(None, pk, None, None, None)
+            secnonce, pubnonce = lib.nonce_gen_internal(RANDOMNESS, None, pk, None, None, None)
 
         no_of_participants.append(no_of_participants_grp)
         pks.append(pk.hex().upper())
@@ -87,39 +90,44 @@ def gen_sign_data():
 
     for _ in range(0, NO_OF_TESTS):
 
-        sk = rnd.randint(1, MAX_KEY_VALUE)
-        pk = lib.cbytes(lib.point_mul(lib.G, sk))
+        sk = secrets.token_bytes(KEY_LENGTH)
+        pk = lib.individual_pk(sk)
 
         # Generate aggpk and coef_a of the group
         no_of_participants_grp = rnd.randint(1, 10)+1
-        sk_grp, pk_grp, current_signer_index = get_sorted_sk_pk(no_of_participants_grp, sk, pk)
+        pk_grp = [get_rand_pk() for _ in range(0, no_of_participants_grp)]
+        pk_grp.append(pk)
+        pk_grp = lib.key_sort(pk_grp)
+        current_signer_index = pk_grp.index(pk)
         coef_a = lib.key_agg_coeff_internal(pk_grp, pk, lib.get_second_key(pk_grp))
         aggpk_grp = lib.key_agg(pk_grp).Q
 
         # Generate aggnonce
         secnonces_grp = []
         pubnonces_grp = []
-        nonces = [lib.nonce_gen(None, pk_cosigner, lib.xbytes(aggpk_grp), None, None) for pk_cosigner in pk_grp]
+        nonces = [lib.nonce_gen_internal(RANDOMNESS, None, pk_cosigner, lib.xbytes(aggpk_grp), None, None) for pk_cosigner in pk_grp]
 
         for nonce in nonces:
             secnonces_grp.append(nonce[0])
             pubnonces_grp.append(nonce[1])
 
+        secnonce_tmp = secnonces_grp[current_signer_index]
         secnonces.append(secnonces_grp[current_signer_index].hex().upper())
         aggnonce = lib.nonce_agg(pubnonces_grp)
 
         # Sign
         message_len = rnd.randint(1, MAX_MESSAGE_LEN)
         message = rnd.randbytes(message_len)
-        psigs = sign_partial(secnonces_grp, sk_grp, aggnonce, pk_grp, message)
-        agg_sig = sig_agg(psigs, aggnonce, pk_grp, message)
+        psig = lib.sign(secnonce_tmp, sk, (aggnonce, pk_grp, [], [], message))
 
-        sks.append(lib.bytes_from_int(sk).hex().upper())
+        assert lib.partial_sig_verify_internal(psig, pubnonces_grp[current_signer_index], pk, (aggnonce, pk_grp, [], [], message))
+
+        sks.append(sk.hex().upper())
         aggpks.append(lib.cbytes(aggpk_grp).hex().upper())
         aggnonces.append(aggnonce.hex().upper())
         coef_as.append(lib.bytes_from_int(coef_a).hex().upper())
         messages.append(message.hex().upper())
-        signatures.append(agg_sig.hex().upper())
+        signatures.append(psig.hex().upper())
         no_of_participants.append(no_of_participants_grp)
 
     out_df = pd.DataFrame({
@@ -137,12 +145,14 @@ def gen_sign_data():
 
 
 def get_sk_pk() -> tuple[int, bytes]:
-    sk = rnd.randint(0, MAX_KEY_VALUE)
-    return sk, lib.cbytes(lib.point_mul(lib.G, sk))
+    sk = secrets.token_bytes(KEY_LENGTH)
+    pk = lib.individual_pk(sk)
+
+    return sk, pk
 
 def get_rand_pk() -> bytes:
-    sk = rnd.randint(0, MAX_KEY_VALUE)
-    return lib.cbytes(lib.point_mul(lib.G, sk))
+    sk = secrets.token_bytes(KEY_LENGTH)
+    return lib.individual_pk(sk)
 
 def get_sorted_sk_pk(no_of_participants_grp: int, sk: int, pk: bytes) -> tuple[list[int], list[bytes], int]:
     '''
@@ -156,24 +166,14 @@ def get_sorted_sk_pk(no_of_participants_grp: int, sk: int, pk: bytes) -> tuple[l
 
     return sk_grp, pk_grp, pk_grp.index(pk)
 
-def sign_partial(secnonces, sks, aggnonce, pubkeys, msg) -> list[bytes]:
-    session_ctx = (aggnonce, pubkeys, [], [], msg)
-    psigs = []
-    
-    for secnonce, sk in zip(secnonces, sks):
-        psig = lib.sign(secnonce, lib.bytes_from_int(sk), session_ctx)
-        psigs.append(psig)
-    
-    return psigs
-
 def sig_agg(psigs, aggnonce, pubkeys, msg):
     session_ctx = (aggnonce, pubkeys, [], [], msg)
     return lib.partial_sig_agg(psigs, session_ctx)
 
 def main():
-    #gen_sign_data()
+    gen_sign_data()
     #gen_keygen_data()
-    gen_noncegen_data()
+    #gen_noncegen_data()
 
 if __name__ == "__main__":
     main()
